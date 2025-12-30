a77c089415e70c145d79718683f5a7e64d974fb9
brenthoang12 <brenthoang@gmail.com>
2025-12-29 20:21:56 -0500

    calibrated accelerometer and roll and pitch

--- esp32_code/src/main.cpp
+++ esp32_code/src/main.cpp
@@ -1,126 +1,128 @@
 #include <Wire.h>
 #include <Adafruit_LIS3MDL.h>
+#include <Adafruit_LSM6DS3TRC.h>
 #include <Adafruit_Sensor.h>
 
 Adafruit_LIS3MDL lis3mdl;
+Adafruit_LSM6DS3TRC lsm6ds3trc;
 
-// Hard-iron calibration
-const float hard_iron[3] = {0.00f, 0.00f, 0.00f};
-// Soft-iron calibration
-const float soft_iron[3][3] = {
-  { 1 , 0 , 0 },
-  { 0 , 1 , 0 },
-  { 0 , 0 , 1 } 
+
+float xMax = 1.00f;
+float xMin = -1.00f;
+float yMax = 1.00f;
+float yMin = -1.01f;
+float zMax = 1.01f;
+float zMin = -0.99f;
+
+float xOffset = (xMax + xMin) / 2.0f;
+float yOffset = (yMax + yMin) / 2.0f;
+float zOffset = (zMax + zMin) / 2.0f;
+float xScale = 2.0f/(xMax - xMin); 
+float yScale = 2.0f/(yMax - yMin); 
+float zScale = 2.0f/(zMax - zMin);
+
+float roll_raw;
+float pitch_raw;
+
+float roll  = 0.0f;
+float pitch = 0.0f;
+
+const float DECLINATION_DEG = -10.17f;
+const float DECLINATION_RAD = DECLINATION_DEG * (PI / 180.0f);
+// const float PHONE_OFFSET_DEG = 0;  
+const float G = 9.81f; // 9.80665f
+
+const float HARD_IRON[3] = {-35.92f, 13.33f, -20.58f};
+const float SOFT_IRON[3][3] = {
+  {  0.973f,  0.047f, -0.109f },
+  {  0.047f,  1.056f,  0.006f },
+  { -0.109f,  0.006f,  0.987f }
 };
 
-
-void setup(void) {
+void setup(void)
+{
   Serial.begin(115200);
-  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+  while (!Serial)
+    delay(10); 
 
-  Serial.println("Adafruit LIS3MDL test!");
-  
-  if (! lis3mdl.begin_I2C()) {          
+  if (!lis3mdl.begin_I2C()) {
     Serial.println("Failed to find LIS3MDL chip");
-    while (1) { delay(10); }
-  }
-  Serial.println("LIS3MDL Found!");
-
-  lis3mdl.setPerformanceMode(LIS3MDL_MEDIUMMODE);
-  Serial.print("Performance mode set to: ");
-  switch (lis3mdl.getPerformanceMode()) {
-    case LIS3MDL_LOWPOWERMODE: Serial.println("Low"); break;
-    case LIS3MDL_MEDIUMMODE: Serial.println("Medium"); break;
-    case LIS3MDL_HIGHMODE: Serial.println("High"); break;
-    case LIS3MDL_ULTRAHIGHMODE: Serial.println("Ultra-High"); break;
+    while (1) {
+      delay(10);
+    }
   }
 
-  lis3mdl.setOperationMode(LIS3MDL_CONTINUOUSMODE);
-  Serial.print("Operation mode set to: ");
-  // Single shot mode will complete conversion and go into power down
-  switch (lis3mdl.getOperationMode()) {
-    case LIS3MDL_CONTINUOUSMODE: Serial.println("Continuous"); break;
-    case LIS3MDL_SINGLEMODE: Serial.println("Single mode"); break;
-    case LIS3MDL_POWERDOWNMODE: Serial.println("Power-down"); break;
-  }
-
-  lis3mdl.setDataRate(LIS3MDL_DATARATE_155_HZ);
-  // You can check the datarate by looking at the frequency of the DRDY pin
-  Serial.print("Data rate set to: ");
-  switch (lis3mdl.getDataRate()) {
-    case LIS3MDL_DATARATE_0_625_HZ: Serial.println("0.625 Hz"); break;
-    case LIS3MDL_DATARATE_1_25_HZ: Serial.println("1.25 Hz"); break;
-    case LIS3MDL_DATARATE_2_5_HZ: Serial.println("2.5 Hz"); break;
-    case LIS3MDL_DATARATE_5_HZ: Serial.println("5 Hz"); break;
-    case LIS3MDL_DATARATE_10_HZ: Serial.println("10 Hz"); break;
-    case LIS3MDL_DATARATE_20_HZ: Serial.println("20 Hz"); break;
-    case LIS3MDL_DATARATE_40_HZ: Serial.println("40 Hz"); break;
-    case LIS3MDL_DATARATE_80_HZ: Serial.println("80 Hz"); break;
-    case LIS3MDL_DATARATE_155_HZ: Serial.println("155 Hz"); break;
-    case LIS3MDL_DATARATE_300_HZ: Serial.println("300 Hz"); break;
-    case LIS3MDL_DATARATE_560_HZ: Serial.println("560 Hz"); break;
-    case LIS3MDL_DATARATE_1000_HZ: Serial.println("1000 Hz"); break;
+  if (!lsm6ds3trc.begin_I2C()) {
+    Serial.println("Failed to find LSM6DS3TR-C chip");
+    while (1) {
+      delay(10);
+    }
   }
   
-  lis3mdl.setRange(LIS3MDL_RANGE_4_GAUSS);
-  Serial.print("Range set to: ");
-  switch (lis3mdl.getRange()) {
-    case LIS3MDL_RANGE_4_GAUSS: Serial.println("+-4 gauss"); break;
-    case LIS3MDL_RANGE_8_GAUSS: Serial.println("+-8 gauss"); break;
-    case LIS3MDL_RANGE_12_GAUSS: Serial.println("+-12 gauss"); break;
-    case LIS3MDL_RANGE_16_GAUSS: Serial.println("+-16 gauss"); break;
-  }
-
+  lis3mdl.setPerformanceMode(LIS3MDL_MEDIUMMODE); 
+  lis3mdl.setOperationMode(LIS3MDL_CONTINUOUSMODE);
+  lis3mdl.setDataRate(LIS3MDL_DATARATE_155_HZ);
+  lis3mdl.setRange(LIS3MDL_RANGE_4_GAUSS); 
   lis3mdl.setIntThreshold(500);
-  lis3mdl.configInterrupt(false, false, true, // enable z axis
-                          true, // polarity
-                          false, // don't latch
-                          true); // enabled!
+  lis3mdl.configInterrupt(false, false, true, true, false, true);              
+  
+  lsm6ds3trc.setAccelRange(LSM6DS_ACCEL_RANGE_2_G); 
+  lsm6ds3trc.setGyroRange(LSM6DS_GYRO_RANGE_500_DPS); // default 2000 DPS
+
+  lsm6ds3trc.setAccelDataRate(LSM6DS_RATE_104_HZ); // default 104 Hz
+  lsm6ds3trc.setGyroDataRate(LSM6DS_RATE_104_HZ); // default 104 Hz
+
+  lsm6ds3trc.configInt1(false, false, true); 
+  lsm6ds3trc.configInt2(false, true, false); 
 }
 
 void loop() {
-  sensors_event_t event;
-  lis3mdl.getEvent(&event);
-  
-  // Fake accel/gyro for MotionCal
-  int ax = 0, ay = 0, az = 0;   
-  int gx = 0, gy = 0, gz = 0;
+  sensors_event_t mag, accel, gyro, temp;
+  lis3mdl.getEvent(&mag);
+  lsm6ds3trc.getEvent(&accel, &gyro, &temp);
 
-  float raw_mag[3] = {event.magnetic.x, event.magnetic.y, event.magnetic.z};
-  float hi_mag[3];
-  float mag_data[3];
-  
-  // Hard-iron calibration
-  for (uint8_t i = 0; i < 3; i++) {
-    hi_mag[i] = raw_mag[i] - hard_iron[i];
-  }
+  float a_raw[3] = {accel.acceleration.x, accel.acceleration.y, accel.acceleration.z};
+  float g_raw[3] = {gyro.gyro.x, gyro.gyro.y, gyro.gyro.z};
+  float m_raw[3] = {mag.magnetic.x, mag.magnetic.y, mag.magnetic.z};
 
-  // Soft-iron calibration
-  for (uint8_t i = 0; i < 3; i++) {
-    mag_data[i] = (soft_iron[i][0] * hi_mag[0]) +
-                  (soft_iron[i][1] * hi_mag[1]) +
-                  (soft_iron[i][2] * hi_mag[2]);
-  }
-
-    // Scale magnetometer values to match MotionCal
-  int mag_data_i[3] = {
-    (int)lroundf(mag_data[0] * 10.0f),
-    (int)lroundf(mag_data[1] * 10.0f),
-    (int)lroundf(mag_data[2] * 10.0f)
+  float a_G[3] = {
+    ((a_raw[0] / G)),
+    ((a_raw[1] / G)),
+    ((a_raw[2] / G))
   };
 
-  Serial.print("Raw:");
-  Serial.print(ax); Serial.print(',');
-  Serial.print(ay); Serial.print(',');
-  Serial.print(az); Serial.print(',');
-  Serial.print(gx); Serial.print(',');
-  Serial.print(gy); Serial.print(',');
-  Serial.print(gz); Serial.print(',');
-  Serial.print(mag_data_i[0]); Serial.print(',');
-  Serial.print(mag_data_i[1]); Serial.print(',');
-  Serial.print(mag_data_i[2]);
-  Serial.println();
+  float a_G_cal[3] = {
+    (a_G[0] - xOffset) * xScale,
+    (a_G[1] - yOffset) * yScale,
+    (a_G[2] - zOffset) * zScale
+  };
 
-  delay(10); 
+  float a_cal[3] = {
+    a_G_cal[0] * G,
+    a_G_cal[1] * G,
+    a_G_cal[2] * G
+  };
+  
+  roll_raw = atan2(a_cal[0],sqrt(a_cal[2]*a_cal[2]+a_cal[1]*a_cal[1]))*360/(2*3.14);
+  pitch_raw = atan2(a_cal[1],sqrt(a_cal[2]*a_cal[2]+a_cal[0]*a_cal[0]))*360/(2*3.14);
+  roll = .8*roll + .2*roll_raw;
+  pitch = .8*pitch + .2*pitch_raw;
+
+  Serial.print("Ax: "); Serial.print(a_cal[0], 1);
+  Serial.print(" Ay: "); Serial.print(a_cal[1], 1);
+  Serial.print(" Az: "); Serial.println(a_cal[2], 1);
+  
+  // int gyro_data_g[3] = {
+  //   (int)lroundf(raw_g[0] * (180.0f / PI)),
+  //   (int)lroundf(raw_g[1] * (180.0f / PI)),
+  //   (int)lroundf(raw_g[2] * (180.0f / PI))
+  // };
+
+  // int mag_data_i[3] = {
+  //   (int)lroundf(mag_data[0] * 10.0f),
+  //   (int)lroundf(mag_data[1] * 10.0f),
+  //   (int)lroundf(mag_data[2] * 10.0f)
+  // };
+
+  delay(10);
 }
-
